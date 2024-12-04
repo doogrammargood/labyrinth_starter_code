744238012e2c18889e71ed002030d050039b7f3a
Victor Bankston <doogrammargood@gmail.com>
2024-12-04 16:55:46 -0600

    added and corrected comments

--- maze_design_algorithms.py
+++ maze_design_algorithms.py
@@ -2,22 +2,23 @@
 #TODO: implement the Fisher Shuffle and Kruskal's algorithm to return
 import numpy as np
 def fisher_shuffle(n):
-    #returns a uniformly random permutation of the list [0..n]
-    #TODO
+    #returns a uniformly random permutation of the list [0..n-1]
+    #you can use  np.random.randint(a) to select a uniformly random number in the range [0..a-1]
     pass
 
 def kruskals(G, edge_weights):
     #expects G to be a networkx graph. 
-    #expects edge_weights to be nonnegative weights of G
+    #expects edge_weights to be dictionary whose keys are nodes and values are nonnegative weights of G
     #returns a networkx spanning tree of G.
-    #TODO
     def get_component(node,components):
         #repeatedly calls component until the answer stabilizes.
         #returns a node in the connected component of node that represents the connected component.
         #updates the values of components for all keys queried along the way to point to this representative.
-        pass
-    for edge in edges:
-        pass
+        #Students may change this to keep track of components differently.
+        comp=None
+        return comp
+    edges = G.edges #the edges of G
+    return G.edge_subgraph(spanning_tree_edges) #returns the subgraph spanned by the edges of G.
 
 def random_spanning_tree(G):
     #expect G to be a networkx graph.
@@ -30,7 +31,7 @@ def random_spanning_tree(G):
 def update_shortest_distances(K,shortest_distances,new_edge,new_edge_distance):
     #assume that
     #K . . . . . . . . . . . a known maze
-    #shortest_distances . . .a default dictionary with default value float('inf') 
+    #shortest_distances . . .a default dictionary with default value infinity
     # . . . . . . . . . . . . . . that has pairs (node1,node2) as keys and shortest distances through the graph as values.
     #new_edge_distance. . . .the new_distance of the node.
     #returns shortest distances after updating it.
@@ -51,7 +52,7 @@ def recover_path_from_shortest_distances(K,shortest_distances,node1,node2):
     #shortest_distances. . . . a default dictionary with default value float('inf')
     # . . . . . . . . . .that has pairs (node1,node2) as keys and shortest distances through the graph as values.
     #node1 and node2 are nodes of K, strings.
-    #returns a list of vertices [node1, ..., node2] that is the shortest path in K.
+    #returns a list of vertices [node1, ..., node2] that is the shortest path in K from node1 to node2
     current_distance = shortest_distances[(node1,node2)]
     if current_distance==float('inf'): #If the distance is infinite...
          assert False #this should not happen.
@@ -68,4 +69,3 @@ def recover_path_from_shortest_distances(K,shortest_distances,node1,node2):
                     break
         to_return.append(node2)
         return to_return
-    
--- maze_solve_algorithms.py
+++ maze_solve_algorithms.py
@@ -1,29 +1,61 @@
 #this file contains the algorithms used in solving the maze
 from maze import *
-import time
 import scipy.optimize
 def depth_first_search_choice(Theseus, string):
     #expects Theseus to be a hero, whose location is a node.
     #string is a list of nodes starting from the entrance to the current location.
+    #If the hero is forced to backtrack, this modifies string by removing its last element.
     #returns the string after the hero makes his next step.
-    #TODO
+    '''
+    Use:
+    Theseus.K.unexplored_nodes() . . . .A list of edges (v,w), where v has been explored but w has not
+    Theseus.location . . . . . . . . . .The current node where Theseus is.
+    Theseus.K.explored.nodes . . . . . .The nodes that Theseus has explored.
+    '''
     pass
+    
 def depth_first_search(Theseus):
     #Theseus is a hero. draws the progress as a side effect.
     #expects drawing to be an instance of the graph_drawer class.
-    #TODO
-    pass
+    '''
+    Use:
+    M.exit . . . . . . . . . . .The node that is the exit of M.
+    Theseus.location. . . . . . The node where Theseus is.
+    Theseus.explore_node(node) .Moves Theseus to node, if it is a neighbor of his location. Has side effects on K and updates the drawing.
+    depth_first_search_choice . Defined above
+    '''
+    M = Theseus.K.M #The maze that Theseus is in.
 
 def a_star_search(Theseus):
     #implements the A* algorithm to search the graph for the exit.
-    #Consider using Theseus.K.unexplored_nodes() to get the edges that lead to a node on the frontier.
-    #TODO
-    pass
+    '''
+    Use: 
+    M.entrance . . . . . . . . . . . . . . The node of M that is an entrance.
+    M.exit . . . . . . . . . . . . . . . . The node of M that is an exit.
+    K.shortest_distances . . . . . . . . . A default dictionary whose keys are pairs of nodes (v,w) and whose value is the length of the shortest path through K from v to w.
+    M.edge_length(node1,node2) . . . . . . Returns the distance between node1 and node2. They don't need to be adjacent.
+    Theseus.travel_to_node(node) . . . . . Takes Theseus to the node v along the shortest path through K.
+    Theseus.explore_node(node) . . . . . . Moves Theseus to node, if it is a neighbor of his location. Has side effects on K and updates the drawing.
+    '''
+    M=Theseus.K.M
+    K=Theseus.K
+    def heuristic_cost(v,node):
+        #Returns the value to minimize in the greedy choice: g(n)+h(n)
+        #where g(n) is the length of the shortest known path from the entrance to n
+        #and h(n) is the length of distance from n to the exit, as the crow flies.
+        return None
+    #Theseus.print_stats()
 
 def linear_programming_shortest_path(M,node1,node2):
     #Assume M is a maze. We assume that we know the whole maze for this method.
     #We write the shortest path as a linear program.
-    #TODO define A,b,c in terms of M.G
+
+    #The edges are directed edge of M.D
+    '''
+    Use: basic list operations: append, and copy.
+    Define a list-of-lists A and lists c and b to express the shortest path problem as a linear program.
+    '''
+    
     #See https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.linprog.html
     result = scipy.optimize.linprog(np.array(c),A_eq=np.array(A), b_eq = np.array(b), bounds = (0,1), method = 'highs')
     return(result['fun'])
